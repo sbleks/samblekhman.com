a27d5dfd354eae9b6de49deb6a3917bad5d0571e
Sam <31251023+sbleks@users.noreply.github.com>
2022-02-16 13:46:23 -0600

    add titles to tech

--- app/components/projectCard.tsx
+++ app/components/projectCard.tsx
@@ -59,12 +59,15 @@ export default function ProjectCard({
                   const iconLink = getTechIcon(tech)
                   if (!iconLink) return null
                   return (
-                    <img
-                      key={tech}
-                      className="h-6 text-white"
-                      src={iconLink}
-                      alt={tech}
-                    />
+                    <>
+                      <img
+                        key={tech}
+                        title={tech}
+                        className="h-6 text-white"
+                        src={iconLink}
+                        alt={tech}
+                      />
+                    </>
                   )
                 })}
               </div>
